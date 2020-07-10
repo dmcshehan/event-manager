74bd8728d353fabb64d0f86d8a6d0329180d0635
Shehan <dmcshehan@outlook.com>
2020-07-10 07:52:30 +0530

    fix eventdropdown is-active

--- src/App/EventControlDropdown/EventControlDropdown.js
+++ src/App/EventControlDropdown/EventControlDropdown.js
@@ -6,23 +6,22 @@ import classNames from "./EventControlDropdown.module.scss";
 export default function EventControlDropdown() {
   return (
     <div>
-      <div className='dropdown is-active is-right'>
+      <div className='dropdown is-right'>
         <div className='dropdown-trigger'>
           <button
             className='button'
             aria-haspopup='true'
             aria-controls='dropdown-menu'
           >
-            <span>Dropdown button</span>
             <span className='icon is-small'>
-              <i className='fas fa-angle-down' aria-hidden='true'></i>
+              <i class='fas fa-ellipsis-h'></i>
             </span>
           </button>
         </div>
         <div className='dropdown-menu' id='dropdown-menu' role='menu'>
           <div className='dropdown-content'>
             <a href='#' className='dropdown-item'>
-              Dropdown item
+              Delete Event
             </a>
           </div>
         </div>
